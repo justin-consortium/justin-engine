b6f9e064d9decbe3dbbcc072a840a46be4f5230a
José-Antonio Rubio <jadrubio@umich.edu>
2026-04-07 15:38:34 -0400

    update to handlers directory

---
+++ src/handlers/index.ts
@@ -0,0 +1,15 @@
+export { HandlerType, DecisionRuleStep, TaskStep } from './types';
+export type {
+  StepStatus,
+  StepReturnResult,
+  BaseHandler,
+  Task,
+  TaskRegistration,
+  DecisionRule,
+  DecisionRuleRegistration,
+  ExecuteStepReturn,
+  RecordResult,
+  RecordResultFunction,
+} from './types';
+
+export { executeStep } from './steps';
